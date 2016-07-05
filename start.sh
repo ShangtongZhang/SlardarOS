1a2d127eb0b8ef542803cd5d1823fcd60a595d58
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2016-07-05 21:12:11 +0800

    Fix scripts

--- start.sh
+++ start.sh
@@ -1,10 +1,2 @@
-<<<<<<< Updated upstream
-qemu-system-i386 -fda ./generated/SlardarOS.img
-=======
 qemu-system-i386 -fda ./generated/SlardarOS.img
 #qemu-system-i386 -s -S -fda ./generated/SlardarOS.img
-#b *0x7d0f
-#b *0x7d14
-#target remote localhost:1234
-#display /10i $pc
->>>>>>> Stashed changes
